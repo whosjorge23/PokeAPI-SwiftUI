d015573ac44bee414c2482a9322c452402543583
Giorgio Giannotta <jorge_upa@msn.com>
2023-02-11 16:40:05 +0100

    Add more PokemonDetails

---
+++ PokeAPI SwiftUI/Models/PokemonModel.swift
@@ -0,0 +1,31 @@
+//
+//  PokemonModel.swift
+//  MVVMPokedex
+//
+//  Created by Federico on 30/03/2022.
+//
+
+import Foundation
+
+
+struct PokemonPage: Codable {
+    let count: Int
+    let next: String
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable, Identifiable, Equatable {
+    let id = UUID()
+    let name: String
+    let url: String
+    
+    static var samplePokemon = Pokemon(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/")
+}
+
+struct DetailPokemon: Codable {
+    let id: Int
+    let height: Int
+    let weight: Int
+}
+
+
--- PokeAPI SwiftUI/Views/PokemonDetailView.swift
+++ PokeAPI SwiftUI/Views/PokemonDetailView.swift
@@ -16,10 +16,35 @@ struct PokemonDetailView: View {
             PokemonView(pokemon: pokemon)
             
             VStack(spacing: 10) {
-                Text("**ID**: \(vm.pokemonDetails?.id ?? 0)")
-                Text("**Type**: \(vm.pokemonDetails?.types[0].type.name.capitalized ?? "Unknown")")
-                Text("**Weight**: \(vm.formatHW(value: vm.pokemonDetails?.weight ?? 0)) KG")
-                Text("**Height**: \(vm.formatHW(value: vm.pokemonDetails?.height ?? 0)) M")
+                
+                HStack {
+                    Text("**ID**: \(vm.pokemonDetails?.id ?? 0)")
+                    Spacer()
+                    Text("**Type**: \(vm.pokemonDetails?.types[0].type.name.capitalized ?? "Unknown")")
+                }
+                HStack {
+                    Text("**Weight**: \(vm.formatHW(value: vm.pokemonDetails?.weight ?? 0)) KG")
+                    Spacer()
+                    Text("**Height**: \(vm.formatHW(value: vm.pokemonDetails?.height ?? 0)) M")
+                }
+                
+//                Text("**\(vm.pokemonDetails?.stats[0].stat.name.capitalized ?? "HP")**: \(vm.pokemonDetails?.stats[0].base_stat ?? 0)")
+                ProgressView("**\(vm.pokemonDetails?.stats[0].stat.name.capitalized ?? "HP")**: \(vm.pokemonDetails?.stats[0].base_stat ?? 0)", value: Double(vm.pokemonDetails?.stats[0].base_stat ?? 0)/100, total: 2.0)
+                
+//                Text("**\(vm.pokemonDetails?.stats[1].stat.name.capitalized ?? "Attack")**: \(vm.pokemonDetails?.stats[1].base_stat ?? 0)")
+                ProgressView("**\(vm.pokemonDetails?.stats[1].stat.name.capitalized ?? "Attack")**: \(vm.pokemonDetails?.stats[1].base_stat ?? 0)", value: Double(vm.pokemonDetails?.stats[1].base_stat ?? 0)/100, total: 2.0)
+                
+//                Text("**\(vm.pokemonDetails?.stats[2].stat.name.capitalized ?? "Defense")**: \(vm.pokemonDetails?.stats[2].base_stat ?? 0)")
+                ProgressView("**\(vm.pokemonDetails?.stats[2].stat.name.capitalized ?? "Defense")**: \(vm.pokemonDetails?.stats[2].base_stat ?? 0)", value: Double(vm.pokemonDetails?.stats[2].base_stat ?? 0)/100, total: 2.0)
+                
+//                Text("**\(vm.pokemonDetails?.stats[3].stat.name.capitalized ?? "Special-Attack")**: \(vm.pokemonDetails?.stats[3].base_stat ?? 0)")
+                ProgressView("**\(vm.pokemonDetails?.stats[3].stat.name.capitalized ?? "Special-Attack")**: \(vm.pokemonDetails?.stats[3].base_stat ?? 0)", value: Double(vm.pokemonDetails?.stats[3].base_stat ?? 0)/100, total: 2.0)
+                
+//                Text("**\(vm.pokemonDetails?.stats[4].stat.name.capitalized ?? "Special-Defense")**: \(vm.pokemonDetails?.stats[4].base_stat ?? 0)")
+                ProgressView("**\(vm.pokemonDetails?.stats[4].stat.name.capitalized ?? "Special-Defense")**: \(vm.pokemonDetails?.stats[4].base_stat ?? 0)", value: Double(vm.pokemonDetails?.stats[4].base_stat ?? 0)/100, total: 2.0)
+                
+//                Text("**\(vm.pokemonDetails?.stats[5].stat.name.capitalized ?? "Speed")**: \(vm.pokemonDetails?.stats[5].base_stat ?? 0)")
+                ProgressView("**\(vm.pokemonDetails?.stats[5].stat.name.capitalized ?? "Speed")**: \(vm.pokemonDetails?.stats[5].base_stat ?? 0)", value: Double(vm.pokemonDetails?.stats[5].base_stat ?? 0)/100, total: 2.0)
                 
             }
             .padding()
