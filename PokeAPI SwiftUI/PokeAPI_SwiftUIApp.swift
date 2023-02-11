bb177fe959ad723d81a7a0c5655fb200d0a29b96
Giorgio Giannotta <jorge_upa@msn.com>
2023-02-11 10:20:09 +0100

    Initial Commit

---
+++ PokeAPI SwiftUI/PokeAPI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPI_SwiftUIApp.swift
+//  PokeAPI SwiftUI
+//
+//  Created by Giorgio Giannotta on 11/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
