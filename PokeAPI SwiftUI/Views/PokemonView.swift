2fa9ce9261bd80035df215177486bd9f84625684
Giorgio Giannotta <jorge_upa@msn.com>
2023-02-19 12:11:54 +0100

    Add Kingfisher library for the images cached

--- PokeAPI SwiftUI/Views/PokemonView.swift
+++ PokeAPI SwiftUI/Views/PokemonView.swift
@@ -6,28 +6,36 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct PokemonView: View {
     @EnvironmentObject var vm: ViewModel
     let pokemon: Pokemon
-    let dimensions: Double = 140
+    let dimensions: Double = 150
     
     var body: some View {
         VStack {
-            AsyncImage(url: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(vm.getPokemonIndex(pokemon: pokemon)).png")) { image in
-                if let image = image {
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: dimensions, height: dimensions)
-                }
-            } placeholder: {
-                ProgressView()
-                    .frame(width: dimensions, height: dimensions)
-                    
-            }
-            .background(.thinMaterial)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
+            KFImage(URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(vm.getPokemonIndex(pokemon: pokemon)).png"))
+                .resizable()
+                .scaledToFit()
+                .frame(width: dimensions, height: dimensions)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+//            AsyncImage(url: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(vm.getPokemonIndex(pokemon: pokemon)).png")) { image in
+//                if let image = image {
+//                    image
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: dimensions, height: dimensions)
+//                }
+//            } placeholder: {
+//                ProgressView()
+//                    .frame(width: dimensions, height: dimensions)
+//                    
+//            }
+//            .background(.thinMaterial)
+//            .clipShape(RoundedRectangle(cornerRadius: 20))
 
             Text("\(pokemon.name.capitalized)")
                 .font(.system(size: 16, weight: .regular, design: .monospaced))
